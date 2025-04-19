16e0ecf4559c34112fc9f89c163078e26e16b754
tbmeans <tbmeans2005@gmail.com>
2025-04-19 01:45:00 -0400

    define page messages and data requests

---
+++ src/models.ts
@@ -0,0 +1,62 @@
+// messageModel.ts
+/*
+   Copyright 2025 Timothy B. Means
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import { PageData } from './pageData';
+import { Consts } from './constan';
+import * as api from './apiRequests';
+
+const messaging = function(
+	route: string,
+	token: string = '',
+	errorMessage: string = '',
+	successMessage: string = '',
+	list: any[] = []
+): PageData {
+	let message: string;
+	let nexRoute: string;
+
+	switch(route.replace('/', '')) {
+		case Consts.NEX1:
+			message = Consts.MSG2;
+			nexRoute = Consts.NEX2;
+			break;
+		case Consts.NEX2:
+			message = Consts.MSG3;
+			nexRoute = Consts.NEX3;
+			break;
+		case Consts.NEX3:
+			message = Consts.MSG4;
+			nexRoute = Consts.NEX4;
+			break;
+		case Consts.NEX4:
+			message = successMessage;
+			nexRoute = Consts.NEX3;
+			break;
+		default:
+			message = errorMessage + Consts.MSG1;
+			nexRoute = Consts.NEX1;
+	}
+
+	return { message, nexRoute, token, list };
+};
+
+const dataRequest = function(query: string): any[] {
+	const list = []/* Object.values(JSON.parse(res).list) */;
+	return list;
+};
+
+export default { messaging, dataRequest }
