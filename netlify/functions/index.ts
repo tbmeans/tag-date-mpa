a1b1e0a09af23f0dbdfc1e30cc0b27ac9c0c8e1c
tbmeans <tbmeans2005@gmail.com>
2025-04-18 23:00:00 -0400

    pure node server in comments

--- netlify/functions/index.ts
+++ netlify/functions/index.ts
@@ -17,23 +17,49 @@
 
 import { Config, Context } from "@netlify/functions";
 import { Consts } from "../../src/constan";
+import AuthCookies from "../../src/authCookies";
+import controllers from "../../src/controllers";
 
-const render = (message: string, nexRoute: string) => {
-	return [
-		Consts.DOCH,
-		Consts.STY1,
-		Consts.BODH,
-		'<main><p>' + message + '</p>',
-		'<a href="' + nexRoute + '">' + Consts.BTN1 + '</a></main>',
-		Consts.FOOT,
-		Consts.DOCC,
-	].join(' ');
-};
-
-// In the Fetch API interface "Request", cookie-get looks like req.headers.get("Cookie")
+/*
+	if (cookieCt1AndIsInvalid && route !== Consts.NEX1 ||
+			cookieCt2AndIsInvalid) {
+		controller = controllers.index;
+	} else if (cookieCt1AndIsInvalid && route === Consts.NEX1) {
+		const data = api.toOAuthCookie(api.setOpts(route),
+				{ consumer_key: process.env.CONSUMER_KEY });
+		if (data[0].includes(Consts.ERR1)) {
+			controller = controllers.index;
+			params[1] = data[0] + '<br>';
+		} else {
+			controller = controllers[route];
+			params[1] = api.extractValue(data[0]);
+			res.setHeader('Set-Cookie', data);
+		}
+	} else if (cookieCt1AndIsValid && route !== Consts.NEX2) {
+		controller = controllers.login;
+		params[1] = cookies[0].split('=')[1];
+	} else if (cookieCt1AndIsValid && route === Consts.NEX2) {
+		const data = api.toOAuthCookie(api.setOpts(route), {
+			consumer_key: process.env.CONSUMER_KEY,
+			code: cookies[0].split('=')[1]
+		});
+		if (data[0].includes(Consts.ERR1)) {
+			controller = controllers.index;
+			params[1] = data[0] + '<br>';
+		} else {
+			controller = controllers[route];
+			res.setHeader('Set-Cookie', data);
+		}
+	} else if (cookieCt2AndIsValid && route !== Consts.NEX3) {
+		controller = controllers[Consts.NEX2];
+		// a different message that says 'restarting app' instead of auth's message?
+	} else if (cookieCt2AndIsValid && route === Consts.NEX3) {
+		// api data request to fill app form etc.
+		controller = controllers.app1;
+	} */
 
 export default async (req: Request, context: Context) => {
-	return new Response(render(Consts.MSG1, Consts.NEX1), {
+	return new Response(controllers.index(req.url), {
 		status: 200,
 		headers: { "Content-Type": "text/html" }
 	});
