bde88a93802bc8067f3e6a90d3808b6d8f1278fa
tbmeans <tbmeans2005@gmail.com>
2025-04-26 01:12:52 -0400

    merged w/misc modules for proper SOC

--- src/controllers.ts
+++ src/controllers.ts
@@ -15,24 +15,132 @@
    limitations under the License.
 */
 
-import models from "./models";
+import * as models from "./models";
 import views from "./views";
 
-const index = (route: string, errorMessage: string = ''): string => {
-	return views.index.render(models.messaging(route, '', errorMessage));
+const cap1 = (a: string): string => a[0].toUpperCase() + a.slice(1);
+
+const printScript = function(
+  route: string,
+  isValidCookies: boolean,
+  isCookiePair: boolean,
+  token: string
+): string {
+  if (route.length === 0) {
+    return '';
+  }
+  if (route === models.Consts.NEX1 && isValidCookies === false) {
+    return [
+      models.Consts.COD1,
+      token,
+      models.Consts.COD2,
+      models.Consts.NEX2,
+      models.Consts.COD3,
+    ].join('');
+  } else if (isValidCookies === false) {
+    return [
+      models.Consts.COD4,
+      models.Consts.COD3,
+    ].join('');
+  } /* else if (route === models.Consts.NEX1 && isCookiePair) {
+    // make a script that redirects to dash
+    return [
+      models.Consts.COD4,
+      models.Consts.NEX3,
+      models.Consts.COD3,
+    ];
+  } else if (route === models.Consts.NEX1) {
+    // therefore this catches 'login' with 1 valid cookie
+    // well we ... really should do same as first case
+    // so that case shouldn't && isValidCOokies === false
+    // but maybe isCookiePair === false
+    // so then next changes to NOT 'login' and INValid cookies
+    // or how bout just series of IFs?
+  }
+  */
+  return '';
 };
 
-const login = (route: string, token: string): string => {
-	return views.login.render(models.messaging(route, token));
+const auth = async function(req: Request): Promise<string[]> {
+  const route: string = req.url.slice(1);  // remove leading '/'
+  const cookies = models.AuthCookies.fromClient(req);
+  const authReqHead1 = models.listParams(models.Consts.NEX1);
+  const authReqHead2 = models.listParams(models.Consts.NEX2);
+  let message: string, link: string, label: string, list: string, js: string;
+
+  if (cookies.isValid() === false) {
+    if (route.length === 0) {
+      // index route
+      message = models.Consts.MSG1;
+      link = models.Consts.NEX1;
+      label = cap1(models.Consts.NEX1);
+      list = '[]';
+      js = '';
+    } else {
+      let cookie = (route === models.Consts.NEX1 &&
+          await models.AuthCookies.fromFetch(authReqHead1) ||
+              new models.AuthCookies([]));
+      /* Only fetch if route is 'login' otherwise send 'em
+      back to index route to hit button and do process right. */
+      message = (route === models.Consts.NEX1 && models.Consts.MSG2 ||
+          models.Consts.MSG1); // BUT if cookie/token has error though, add error print-out to message and go back to index.
+      link = '';
+      label = '';
+      list = '[]';
+      js = printScript(route, false, false, cookie.value(0));      
+    } 
+  } else if (cookies.isPair() === false) {} else {}
+/*
+	if (cookieCt1AndIsInvalid && route !== Consts.NEX1 ||
+			cookieCt2AndIsInvalid) {
+		controller = controllers.index;
+	} else if (cookieCt1AndIsInvalid && route === Consts.NEX1) {
+		const data = api.toOAuthCookie(api.setOpts(route),
+				{ consumer_key: process.env.CONSUMER_KEY });
+		if (data[0].includes(Consts.ERR1)) {
+			controller = controllers.index;
+			params[1] = data[0] + '<br>';
+		} else {
+			controller = controllers[route];
+			params[1] = api.extractValue(data[0]);
+			res.setHeader('Set-Cookie', data);
+		}
+	} else if (cookieCt1AndIsValid && route !== Consts.NEX2) {
+		controller = controllers.login;
+		params[1] = cookies[0].split('=')[1];
+	} else if (cookieCt1AndIsValid && route === Consts.NEX2) {
+		const data = api.toOAuthCookie(api.setOpts(route), {
+			consumer_key: process.env.CONSUMER_KEY,
+			code: cookies[0].split('=')[1]
+		});
+		if (data[0].includes(Consts.ERR1)) {
+			controller = controllers.index;
+			params[1] = data[0] + '<br>';
+		} else {
+			controller = controllers[route];
+			res.setHeader('Set-Cookie', data);
+		}
+	} else if (cookieCt2AndIsValid && route !== Consts.NEX3) {
+		controller = controllers[Consts.NEX2];
+		// a different message that says 'restarting app' instead of auth's message?
+	} else if (cookieCt2AndIsValid && route === Consts.NEX3) {
+		// api data request to fill app form etc.
+		controller = controllers.app1;
+	} */
+
+	return [ "hello" ];
 };
 
-const auth = (route: string): string => {
-	return views.auth.render(models.messaging(route));
+const apiq = async function(route: string, token: string): Promise<string[]> {
+    // Use models classes to consume api endpoint
+    return [ "world" ];
+}
+
+const resp = function(route: string, cookieList: string[]): string {
+    // Given a route param and cookie in data param, 
+    // put together script if nec.
+    // JSON.parse the list param before plugging into PageData constructor
+	return "!";
 };
 
-const app1 = (route: string, data: any[]): string => {
-    const list = models.dataRequest(route, data);
-	return views.app1.render(models.messaging(route, '', err, suc, list));
-};
-
-export default { index, login, auth, app1, }
+export default { auth, apiq, resp, }
--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,4 @@
-// messageModel.ts
+// models.ts
 /*
    Copyright 2025 Timothy B. Means
 
@@ -15,48 +15,197 @@
    limitations under the License.
 */
 
-import { PageData } from './pageData';
 import { Consts } from './constan';
-import * as api from './apiRequests';
 
-const messaging = function(
+export { Consts }; 
+
+export interface FetchParams {
+  input: string;
+  options: RequestInit;
+}
+
+export interface V3Action {
+	action: string;
+	item_id: string;
+	tags?: string;
+	time?: string;
+}
+
+export interface V3Body {
+	consumer_key?: string;
+	redirect_uri?: string;
+	code?: string;
+	access_token?: string;
+	sort?: string;
+	since?: number;
+	tag?: string;
+	count?: string; /* endpoint doc's table says integer but example uses stringified number */
+	offset?: string; /* endpoint doc's table says integer but example uses stringified number */
+	total?: string; /* endpoint doc's table says integer but example uses stringified number */
+	detailType?: string;
+	actions?: V3Action[];
+}
+
+export function listParams(
 	route: string,
 	token: string = '',
-	errorMessage: string = '',
-	successMessage: string = '',
-	list: any[] = []
-): PageData {
-	let message: string;
-	let nexRoute: string;
+	data: V3Body = {}
+): FetchParams {
+	const name = route.replace('/', '');
+	const names: string[] = [
+		Consts.NEX1,
+		Consts.NEX2,
+		Consts.NEX3,
+		Consts.NEX4,
+	];
+	const endpts: string[] = [
+		'/oauth/request',
+		'/oauth/authorize',
+		'/get',
+		'/send',
+	];
+	const bods: V3Body[] = [
+		{
+			consumer_key: process.env.CONSUMER_KEY,
+			redirect_uri: Consts.ADDR + Consts.NEX2
+		},
+		{
+			consumer_key: process.env.CONSUMER_KEY,
+			code: token
+		},
+		{
+			consumer_key: process.env.CONSUMER_KEY,
+			access_token: token,
+			sort: Consts.PRF1,
+			since: data.since,
+			tag: data.tag,
+			count: Consts.PRF2,  /* if it turns out really do need int value just parseInt() the string in constants */
+			offset: data.offset, /* parseint if really needs a number */
+			total: Consts.PRF3, /* parseint if really needs a number */
+			detailType: Consts.PRF4
+		},
+		{
+			consumer_key: process.env.CONSUMER_KEY,
+			access_token: token,
+			actions: data.actions
+		},
+	];
+	return {
+		input: "https://getpocket.com/v3" + endpts[names.indexOf(name)],
+		options: {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8',
+				'X-Accept': 'application/json',
+			},
+			body: JSON.stringify(bods[names.indexOf(name)])
+		}
+	};
+}
 
-	switch(route.replace('/', '')) {
-		case Consts.NEX1:
-			message = Consts.MSG2;
-			nexRoute = Consts.NEX2;
-			break;
-		case Consts.NEX2:
-			message = Consts.MSG3;
-			nexRoute = Consts.NEX3;
-			break;
-		case Consts.NEX3:
-			message = Consts.MSG4;
-			nexRoute = Consts.NEX4;
-			break;
-		case Consts.NEX4:
-			message = successMessage;
-			nexRoute = Consts.NEX3;
-			break;
-		default:
-			message = errorMessage + Consts.MSG1;
-			nexRoute = Consts.NEX1;
+export class AuthCookies {
+	list: string[];
+
+	constructor(list: string[]) {
+		this.list = list;
 	}
 
-	return { message, nexRoute, token, list };
-};
+	static fromClient(req: Request) {
+		const cookiesJoined = req.headers.get("Cookie");
+		if (cookiesJoined == null) {
+			return new AuthCookies([ 'null' ]);
+		}
+		return new AuthCookies(cookiesJoined.split(';'));
+	}
 
-const dataRequest = function(query: string): any[] {
-	const list = []/* Object.values(JSON.parse(res).list) */;
-	return list;
-};
+	static async fromFetch(params: FetchParams): Promise<AuthCookies> {
+		const { input, options } = params;
+		const keys = [ Consts.KEY1, Consts.KEY2 ];
+		const vals: string[] = [];
+		try {
+			const res = await fetch(input, options);
+			if (res.ok === false) {
+				const errmsg: string = `${Consts.ERR1} status ${res.status}`;
+				vals[0] = errmsg;
+				throw new Error(errmsg);
+			} else {
+				const jsn = await res.json();
+				const [ dat1, dat2 ]: string[] = Object.values(JSON.parse(jsn));
+				vals[0] = dat1;
+				vals[1] = dat2;
+			}
+		} catch (err) {
+			vals[0] = `${Consts.ERR1} error ${err.message}`;
+		}
+		if (vals[1]) {
+			vals.map((v, x) => `${keys[x]}=${v}; Secure; HttpOnly`);
+		}
+		return new AuthCookies(vals);
+	}
 
-export default { messaging, dataRequest }
+	value(index: number) {
+		if (this.isValid() === false) {
+			return '';
+		}
+		const cookie = this.list[index] ?? '=;';
+		return cookie.slice(cookie.indexOf('=') + 1, cookie.indexOf(';'))
+	}
+
+	isValid(): boolean {
+		if (this.list.length === 0 || this.list.length > 2) {
+			return false;
+		}
+		const results: boolean[] = this.list.map((cookie, x) => {
+			const [ key, val ] = cookie.split('=');
+			const word = [ Consts.KEY1, Consts.KEY2 ][x];
+			return key === word && val?.length > 0;
+		});
+		return results.reduce((p, q) => p && q);
+	}
+
+	isPair(): boolean {
+		return this.list.length === 2;
+	}
+}
+
+export class PageData {
+	message: string;
+	buttonLink: string;
+	buttonLabel: string;
+	list: object[] | string[];
+	jscript: string;
+	isButton: boolean;
+	isList: boolean;
+	isScript: boolean;
+
+	constructor(
+		message: string,
+		buttonLink: string,
+		buttonLabel: string,
+		list: object[] | string[],
+		jscript: string
+	) {
+		this.message = message;
+		this.buttonLink = buttonLink;
+		this.buttonLabel = buttonLabel;
+		this.list = list;
+		this.jscript = jscript;
+		this.isButton = buttonLink.length > 0 && buttonLabel.length > 0;
+		this.isList = list.length > 0;
+		this.isScript = jscript.length > 0;
+	}
+
+	static async printList(params: FetchParams): Promise<object[]> {
+		// this truly belongs in model, it's sourcing data from api endpoints
+		/* const { input, options } = params;
+		let res: any;
+		try {
+			res = await fetch(input, options);
+		} catch (err) {
+		 	res = `{"status":0,"list":{"1234567":{"error":${err.message}}}}`;
+		}
+		 */
+		const res = '{"status":1,"list":{"1234567":{}}}';
+		return Object.values(JSON.parse(res).list);
+	}
+}
