fb51114fb4dafd7f1547c7f893b89b79b4a80fc9
tbmeans <tbmeans2005@gmail.com>
2025-04-27 03:35:03 -0400

    wrap render for export

--- src/views.ts
+++ src/views.ts
@@ -17,37 +17,41 @@
 import { PageData } from "./models";
 import { Consts } from "./constan";
 
-export default function render(data: PageData): string {
-	const btnOpt = [
-		`<a href="${data.buttonLink}">`,
-		`<button type="button">${data.buttonLabel}</button> </a>`,
-	].join(' ');
-
-	const uibod = (list: any[]): string => {
-		console.log(Object.keys(list[0])); 
-		// I want to know if Pocket has a timestamp of when each item was saved. API Docs don't say.
-		if (typeof list[0] === 'string') {
-			// It's an error message.
-			return `<p>${list[0]}</p>`;
-		}
-		return list.map(item => {
-			return [
-				`<p id="${item.item_id}">`,
-				`<a href="${item.given_url}">${item.given_title}</a> </p>`
-			].join(' ');
-		}).join(' ');
-	};
-
-	return [
-		Consts.DOCH,
-		Consts.STY1,
-		Consts.BODH,
-		`<main> <p>${data.message}</p>`,
-		data.isButton && btnOpt || '',
-		data.isList && uibod(data.list) || '',
-		'</main>',
-		data.isScript && `<script> ${data.jscript} </script>` || '',
-		Consts.FOOT,
-		'</body></html>',
-	].join(' ');
+export default {
+	render(data: PageData): string {
+		const btnOpt = [
+			`<a href="${data.buttonLink}">`,
+			`<button type="button">${data.buttonLabel}</button> </a>`,
+		].join(' ');
+	
+		const uibod = (list: any[]): string => {
+			console.log(Object.keys(list[0])); 
+			// I want to know if Pocket has a timestamp of when each item was saved. API Docs don't say.
+			if (typeof list[0] === 'string') {
+				// It's an error message.
+				return `<p>${list[0]}</p>`;
+			}
+			return list.map(item => {
+				return [
+					`<p id="${item.item_id}">`,
+					`<a href="${item.given_url}">${item.given_title}</a> </p>`
+				].join(' ');
+			}).join(' ');
+		}; 
+		// TO-DO: ACTION BUTTONS HREF V3/GET AND V3/SEND ENDPOINTS WITH X-WWW-URLENCODED ENDPOINT REQ PARAMS
+		// BUT JUST MARKUP BUTTONS HERE. CALCULATING THE HREF IS CONTROLLER CONCERN.
+	
+		return [
+			Consts.DOCH,
+			Consts.STY1,
+			Consts.BODH,
+			`<main> <p>${data.message}</p>`,
+			data.isButton && btnOpt || '',
+			data.isList && uibod(data.list) || '',
+			'</main>',
+			data.isScript && `<script> ${data.jscript} </script>` || '',
+			Consts.FOOT,
+			'</body></html>',
+		].join(' ');
+	},
 }
