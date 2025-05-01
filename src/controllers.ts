2364a6e34fbd175ef57be67e0eb41277ccc2a5d7
tbmeans <tbmeans2005@gmail.com>
2025-04-30 22:35:42 -0400

    begin to form get query

--- src/controllers.ts
+++ src/controllers.ts
@@ -28,7 +28,8 @@ const stateHandler = async function(req: Request): Promise<string[]> {
   if (cookies.isValid() === false && route.length > 0 &&
       route !== models.Consts.NEX1) {
     // A controller consisting of a redirect to index serves as an alternative
-    // controller for all routes when request reports no/invalid cookies.
+    // controller for all routes except 'login' (the only route that I'm
+    // allowing direct surf to w/o auth) when request reports no/invalid cookies.
     return [
       models.Consts.MSG1,
       '',
@@ -39,8 +40,12 @@ const stateHandler = async function(req: Request): Promise<string[]> {
         models.Consts.COD3,
       ].join(''),
     ];
-  } else if (cookies.isValid() === false && route.length === 0) {
+  } 
+  
+  if (cookies.isValid() === false && route.length === 0) {
     // Handle index route controller for a fresh session.
+    // Displays a button control to go to 'login' route,
+    // or the user can directly surf to '/login'.
     return [
       models.Consts.MSG1,
       models.Consts.ADDR + models.Consts.NEX1,
@@ -80,9 +85,10 @@ const stateHandler = async function(req: Request): Promise<string[]> {
       models.Consts.NEX2,
       models.Consts.COD3,
     ].join(''),
-    JSON.stringify(token) /* controller's responder needs this to Set-Cookie
-    but this full string for Set-Cookie does not go in pageData, only the 
-    hash value for script to redir to Pocket goes into pageData. */
+    JSON.stringify(token) /* Controller's responder needs this hash string
+    token for Set-Cookie but the Set-Cookie key and "Secure HttpOnly" added
+    on to the hash value does not go in pageData, only the hash value, which
+    goes into the script to redir to Pocket. */
   ];
 };
  
@@ -91,16 +97,16 @@ const stateHandler2 = async function(req: Request): Promise<string[]> {
   const cookies = models.AuthCookies.fromClient(req);
   const authReq = models.listParams(models.Consts.NEX2);
 
-  // We should be here only because stateHandler
-  // determined that the cookies set is valid.
+  // We should be here only because stateHandler determined that the set of
+  // cookies is valid, either one cookie or two.
+  // When requesting any route other than auth with the single first cookie,
+  // don't "re-Set-Cookie," but just let the '/login' message and redirect to
+  // Pocket script appear: just make sure of a proper login to Pocket account.
   // Alt controls for whatever route was dialed: redirect to login route.
-  // BUT HOLD UP NO WE DON'T WANT TO DO LOGIN'S PROCESS OF RE-COOKIE-ING
-  // SO HOW ABOUT LET LOGIN'S CONTROLS APPEAR W/ REDIR TO POCKET, FOR WHATEVER
-  // ROUTE WAS DIALED, AFTERALL. BUT DON'T GIVE A VALUE FOR SET-COOKIE AND
-  // GET THE ONE EXISTING COOKIE'S VALUE FROM REQUEST.
-  // you could only dial a route with one valid cookie if INTERRUPTING
-  // the time delay on the proper login route, so this will make dialing
-  // another route from 'login' look like it does nothin but change address bar
+  // You could only dial a route with one valid cookie if INTERRUPTING
+  // the time delay on the cookie-setting login route, so this will make
+  // dialing another route from 'login' look like it does nothin but change
+  // the address bar.
   if (cookies.isPair() === false && route !== models.Consts.NEX2) {
     return [
       models.Consts.MSG2,
@@ -123,7 +129,7 @@ const stateHandler2 = async function(req: Request): Promise<string[]> {
 
   const tokens = await models.AuthCookies.fromFetch(authReq);
 
-  // Auth route will show button to try again
+  // Auth route will show button to try again in this case.
   // Manually skipping pocket login by direct-dialing my app's
   // 'auth' route SHOULD generate an error per API design/docs.
   if (tokens[0].includes(models.Consts.ERR1)) {
@@ -153,12 +159,42 @@ const stateHandler2 = async function(req: Request): Promise<string[]> {
 
 const stateHandler3 = async function(req: Request): Promise<string[]> {
   const route: string = req.url.slice(1);  // remove leading '/'
-  const dataReq = models.listParams(models.Consts.NEX3);
+  const qpos = route.indexOf('?');
+  const isQuery = qpos >= 0;
+  const cookies = models.AuthCookies.fromClient(req);
+  const urObj = new URL(req.url);
+  const qparm = new URLSearchParams(urObj.search);
+  const firstOfCurMonth = (): number => {
+    const date = new Date();
+    return new Date(...[
+      date.getFullYear(),
+      date.getMonth(),
+      1
+    ]).valueOf(); // unix time stamp
+  };
+  const dataReq = models.listParams(...[
+    route.slice(0, qpos),
+    '',
+    {
+      consumer_key: process.env.CONSUMER_KEY,
+      access_token: cookies.value(0),
+      sort: "oldest",
+      since: firstOfCurMonth(),
+      /*
+      count?: string; endpoint doc's table says integer but example uses stringified number
+	    offset?: string; endpoint doc's table says integer but example uses stringified number
+	    total?: string; endpoint doc's table says integer but example uses stringified number
+	    detailType?: string;
+      */
+    }
+  ]);
+  // const modSend = models.listParams(...[])
 
-  // We should be here only because prior stateHandlers
-  // determined that the cookies set is valid and count of 2.
-  if (route !== models.Consts.NEX3) {
-    // fully auth'd but requested route besides 'dash'
+  // We should be here only because prior stateHandlers determined that the set
+  // of cookies is valid w/count of 2.
+
+  if (route !== models.Consts.NEX3 && route !== models.Consts.NEX4) {
+    // Fully auth'd but requested route besides 'dash'
     // well then send them to dash where they belong
     return [
       models.Consts.MSG4,
@@ -167,17 +203,18 @@ const stateHandler3 = async function(req: Request): Promise<string[]> {
       '[]',
       [
         models.Consts.COD4,
-        models.Consts.NEX3,
+        models.Consts.NEX3, // + query params ?
         models.Consts.COD3,
       ].join(''),
     ];
-  } /* else if (???) {
-    The above is not appropriate return value for the confirm route.
+  } 
+  
+  if (route === models.Consts.NEX4) {
     // TO-DO: FIT INTO RETURN DATA HREF FOR BUTTONS TO USE V3/SEND ENDPOINT
     // THE HREFS WILL INCLUDE X-WWW-URLENCODED ENDPOINT REQ PARAMS.
     // AND UNIX TIMESTAMP WIDGET FOR V3/GET ENDPOINT.
 		// BUT JUST MARKUP BUTTONS HERE. CALCULATING THE HREF IS CONTROLLER CONCERN.
-  } */
+  }
 
   // regular 'dash' route response
   const list = await models.PageData.printList(dataReq);
